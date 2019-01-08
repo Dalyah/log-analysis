d09aa8d33dbc76190b91c1db4d46f10a99b06a54
DalyahAljamal <dalyah_se@outlook.com>
2019-01-08 01:26:11 -0500

    modifications

--- README.md
+++ README.md
@@ -2,8 +2,15 @@
 This project uses news database to analyze user experience and figure out the popular articles and authors.
 It also shows the days with the most request errors.
 
-# The views
-For ease, these views were created.
+# Requirements
+The following are required to run the project.
+1. Python 3
+2. PostgreSQL
+3. NEWS database from https://d17h27t6h515a5.cloudfront.net/topher/2016/August/57b5f748_newsdata/newsdata.zip. After downloading the database, unzip the folder and put newsdata.sql in your PostgreSQL working directory and run the following command in the shell to load the data: psql -d news -f newsdata.sql
+
+
+# Running the project
+To run the project, create the following SQL views using the queries below:
 
 ## article_log View
  CREATE VIEW article_log AS select * from log where path like '%article/%';
@@ -15,3 +22,4 @@ CREATE VIEW article_count AS select path , count(*) as views from article_log gr
 CREATE VIEW author_count AS  select article_count.path, article_count.views, authors.name as author from article_count,
 articles, authors WHERE substring(article_count.path from 10) = articles.slug and authors.id = articles.author;
 
+After you create the views, you can run the project by running: python report.py
--- report.py
+++ report.py
@@ -1,3 +1,5 @@
+#!/usr/bin/env python3
+
 import psycopg2
 import bleach
 
@@ -8,13 +10,6 @@ def get_popular_articles():
     """ Returns the most three popular articles from news db"""
     db = psycopg2.connect(database=DBNAME)
     c = db.cursor()
-    # #Create article_log view
-    # c.execute("CREATE VIEW article_log AS select * from log where path like"+
-    # " '%article/%';");
-    # #Create article_count view
-    # c.execute("CREATE VIEW article_count AS select path , count(*) as views"+
-    # "from article_log group by path order by views DESC;");
-    # c.commit()
     # popular articles
     c.execute(
         "select substring(path from 10), views from article_count limit 3;")
@@ -27,12 +22,6 @@ def get_popular_authors():
     """ Returns the most three popular authors from news db"""
     db = psycopg2.connect(database=DBNAME)
     c = db.cursor()
-    # #Create author_count view
-    # c.execute("CREATE VIEW author_count AS  select article_count.path,"+
-    # "article_count.views, authors.name as author from article_count, "+
-    # "articles, authors WHERE substring(article_count.path from 10) = +"
-    # articles.slug and authors.id = articles.author;");
-    # c.commit()
     # popular authors
     c.execute(
         "select author, sum(views) author_views from author_count" +
@@ -73,6 +62,8 @@ if __name__ == '__main__':
         print author
 
     print "\nDays with more than 1% request errors:"
+    if not get_error_days():
+        print "No Days with more than 1% request errors"
     for day in get_error_days():
         day_err = str(day[0]) + "--" + str(day[1]) + " % errors."
         print day_err
